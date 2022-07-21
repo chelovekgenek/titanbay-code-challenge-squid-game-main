5b4cff32ea5dd9bbf56e0826ff0c39f9d04b5664
Evghenii Notarius <dufa456@gmail.com>
2022-07-21 11:47:19 +0100

    refactor: store player number as 3 digit string

--- src/models/player.ts
+++ src/models/player.ts
@@ -3,24 +3,22 @@ import { DataTypes, ModelAttributes } from 'sequelize'
 import { BaseAttributes, BaseModel, baseModelAttributes } from './base'
 
 export interface PlayerAttributes extends BaseAttributes {
-  number: number
+  number: string // 001
   name: string
 }
 
 export class Player extends BaseModel implements PlayerAttributes {
-  public number: number
+  public number: string
   public name: string
 }
 
-export const playerModelAttributes: ModelAttributes<
-  Player,
-  PlayerAttributes
-> = {
-  ...baseModelAttributes,
-  number: {
-    type: DataTypes.INTEGER
-  },
-  name: {
-    type: DataTypes.TEXT
+export const playerModelAttributes: ModelAttributes<Player, PlayerAttributes> =
+  {
+    ...baseModelAttributes,
+    number: {
+      type: DataTypes.CHAR(3),
+    },
+    name: {
+      type: DataTypes.TEXT,
+    },
   }
-}
--- test/factories/players.js
+++ test/factories/players.js
@@ -5,32 +5,38 @@ const { camelToSnakeCase } = require('./utils')
 
 // Factories aren't in Typescript so that they can be used in seeders via Sequelize CLI (within docker container)
 
+const toDigitStr = (num, length = 3) => {
+  let numstr = String(num)
+  while (numstr.length < length) {
+    numstr = '0' + numstr
+  }
+  return numstr
+}
+
 const createPlayer = (testCase = false) => {
   let player = {
     uid: uuidv4(),
-    number: Faker.datatype.number({
-      'min': 1,
-      'max': 456
-    }),
+    number: toDigitStr(
+      Faker.datatype.number({
+        min: 1,
+        max: 999,
+      })
+    ),
     name: Faker.name.firstName() + ' ' + Faker.name.lastName(),
     createdAt: new Date(),
-    updatedAt: new Date()
+    updatedAt: new Date(),
   }
 
   return !testCase ? camelToSnakeCase(player) : player
 }
 
 const createPlayers = (amount, testCase = false) => {
-  let players = [...Array(amount)].map(() => createPlayer(testCase))
-
-  players.forEach((player, i) => {
-    player.number = i + 1
-  })
+  const players = [...Array(amount)].map(() => createPlayer(testCase))
 
   return players
 }
 
 module.exports = {
   createPlayer,
-  createPlayers
+  createPlayers,
 }
